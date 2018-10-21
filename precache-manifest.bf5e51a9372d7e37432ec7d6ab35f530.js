4fa3f663c969b61fa1df05d7b027f1ae2bd15221
Luke Taylor <lmcjt@outlook.com>
2018-10-21 13:41:07 +0100

    Updates

---
+++ precache-manifest.bf5e51a9372d7e37432ec7d6ab35f530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a549d00ef798d71c5d9",
+    "url": "/curated-tv-and-film/static/js/runtime~main.5a549d00.js"
+  },
+  {
+    "revision": "8f51810e094323845476",
+    "url": "/curated-tv-and-film/static/js/main.8f51810e.chunk.js"
+  },
+  {
+    "revision": "a0fa3eadd388aa6da975",
+    "url": "/curated-tv-and-film/static/js/1.a0fa3ead.chunk.js"
+  },
+  {
+    "revision": "8f51810e094323845476",
+    "url": "/curated-tv-and-film/static/css/main.3154e549.chunk.css"
+  },
+  {
+    "revision": "a0fa3eadd388aa6da975",
+    "url": "/curated-tv-and-film/static/css/1.1670841c.chunk.css"
+  },
+  {
+    "revision": "1ece48aa6f2500265294155152cd87b9",
+    "url": "/curated-tv-and-film/index.html"
+  }
+];
