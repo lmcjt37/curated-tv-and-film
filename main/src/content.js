376a938096f59692942f859f32c7a737ed09d1c3
Ricardo Barba Hernández <44454476+Richb17@users.noreply.github.com>
2018-10-28 13:38:30 -0600

    Update content.js
    Added a new movie to content.js

--- main/src/content.js
+++ main/src/content.js
@@ -238,7 +238,15 @@ var content = [
 		imdb: "https://www.imdb.com/title/tt1037705/",
 		url: "https://www.youtube.com/watch?v=kAMUv22y1og",
 		thumbnail: "./assets/the-book-of-eli.jpg"
-	}
+	}, 
+		type: "movie",
+		title: "Sing Street",
+		description: "A boy growing up in Dublin during the 1980s escapes his strained family life by starting a band to impress the mysterious girl he likes."
+		year: 2016
+		imbd: "https://www.imdb.com/title/tt3544112/"
+		url: "https://www.youtube.com/watch?v=jYk2Vx1z6lk"
+		thumbnail: "./assets/Sing-Street.jpg"
+	},
 ];
 
 export default content;
