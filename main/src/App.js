27b5f7c1036e9ffa5a2b795d9efa5b4463ef42b1
Jamil Hijjawi <j.hijjawi@gmail.com>
2018-10-28 14:43:00 +0200

    Move to top when click on the logo or head text

--- main/src/App.js
+++ main/src/App.js
@@ -18,86 +18,91 @@ class App extends Component {
 
   handleChange = (event) => {
     var searchString = event.target.value;
-    this.setState({searchString: searchString});
+    this.setState({ searchString: searchString });
 
     searchString = searchString.trim().toLowerCase();
 
-    if(searchString.length > 0) {
+    if (searchString.length > 0) {
       var searchResult = this.state.content.filter((el) =>
         el.title.toLowerCase().match(searchString));
-      this.setState({content: searchResult});
+      this.setState({ content: searchResult });
     } else {
-      this.setState({content: data});
+      this.setState({ content: data });
     }
   }
 
   handleFilter = (event) => {
-    this.setState({ [event.target.name] : event.target.checked});
+    this.setState({ [event.target.name]: event.target.checked });
+  }
+
+  goTop = (event) => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
 
   render() {
     return (
       <div className="mainContainer">
-          <div className="headerContainer">
+        <div className="headerContainer">
           <div className="logoContainer">
-            <div className="logoImage">Curated TV and Film</div>
+            <div className="logoImage" onClick={this.goTop}>Curated TV and Film</div>
           </div>
           <div className="content-filter">
-            <input type="text" className="form-control" value={this.state.searchString} onChange={this.handleChange} placeholder="Search here"/>
+            <input type="text" className="form-control" value={this.state.searchString} onChange={this.handleChange} placeholder="Search here" />
           </div>
           <div className='filter-div'>
-              <input type="checkbox" name = "filterMovies" checked={this.state.filterMovies} onChange={this.handleFilter}/><label htmlFor='movies' className="filter-labels">Movies</label>
-              <input type="checkbox" name = "filterTV" checked={this.state.filterTV} onChange={this.handleFilter}/><label htmlFor='tv' className="filter-labels">TV Series</label>
+            <input type="checkbox" name="filterMovies" checked={this.state.filterMovies} onChange={this.handleFilter} /><label htmlFor='movies' className="filter-labels">Movies</label>
+            <input type="checkbox" name="filterTV" checked={this.state.filterTV} onChange={this.handleFilter} /><label htmlFor='tv' className="filter-labels">TV Series</label>
           </div>
         </div>
-      <section className="app container">
-        <main className="main-content">
-          <div className="content-list">
-            {this.state.content.length ? (
-              <ul>
-                {this.state.content.map((item, index) => {
-                  if (this.state.filterMovies === false && item.type === "movie") 
+        <section className="app container">
+          <main className="main-content">
+            <div className="content-list">
+              {this.state.content.length ? (
+                <ul>
+                  {this.state.content.map((item, index) => {
+                    if (this.state.filterMovies === false && item.type === "movie")
                       return "";
-                  if (this.state.filterTV === false && item.type === "tv_show" )  
-                     return "";
-                  return (
-                    <li key={index}>
-                      <div className="item-card">
-                        <div className="item-thumbnail">
-                          <a target="_blank"  rel="noopener noreferrer" href={item.url}>
-                            <img src={item.thumbnail} alt={item.title} className="item-image"/>
-                          </a>
-                        </div>
-                        <div className="item-info">
-                          <h2>{item.title}{item.year ? (" (" + item.year + ")") : ''}</h2>
-                          {item.type === "tv_show" &&
-                            <div className="tvshow-details">
-                              <div className="details__item"><span>Season:</span> {item.season}</div>
-                              <div className="details__item"><span>Episode:</span> {item.episode}</div>
-                              <div className="details__item"><span>Episode Title:</span> {item.episode_title}</div>
+                    if (this.state.filterTV === false && item.type === "tv_show")
+                      return "";
+                    return (
+                      <li key={index}>
+                        <div className="item-card">
+                          <div className="item-thumbnail">
+                            <a target="_blank" rel="noopener noreferrer" href={item.url}>
+                              <img src={item.thumbnail} alt={item.title} className="item-image" />
+                            </a>
+                          </div>
+                          <div className="item-info">
+                            <h2>{item.title}{item.year ? (" (" + item.year + ")") : ''}</h2>
+                            {item.type === "tv_show" &&
+                              <div className="tvshow-details">
+                                <div className="details__item"><span>Season:</span> {item.season}</div>
+                                <div className="details__item"><span>Episode:</span> {item.episode}</div>
+                                <div className="details__item"><span>Episode Title:</span> {item.episode_title}</div>
+                              </div>
+                            }
+                            <div className="item-desc details__item"><span>Description:</span> {item.description}</div>
+                            <div className="item-imdb">
+                              <span>IMDB:</span><a target="_blank" rel="noopener noreferrer" href={item.imdb} className="item-link">{item.imdb}</a>
                             </div>
-                          }
-                          <div className="item-desc details__item"><span>Description:</span> {item.description}</div>
-                          <div className="item-imdb">
-                            <span>IMDB:</span><a target="_blank"  rel="noopener noreferrer" href={item.imdb} className="item-link">{item.imdb}</a>
                           </div>
                         </div>
-                      </div>
-                    </li>
-                  )
-                })}
-              </ul>
+                      </li>
+                    )
+                  })}
+                </ul>
               ) : (
-              this.state.searchString ? (
-                <p>No search result</p>
-                ) : (
-                <p>Can't load the data.</p>
+                  this.state.searchString ? (
+                    <p>No search result</p>
+                  ) : (
+                      <p>Can't load the data.</p>
+                    )
                 )
-              )
-            }
-          </div>
-        </main>
-      </section>
+              }
+            </div>
+          </main>
+        </section>
       </div>
     );
   }
