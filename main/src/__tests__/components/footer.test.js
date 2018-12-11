bc6b333baaa5d982c9082527c2fc017b921135d0
Luke Taylor <lmcjt@outlook.com>
2018-12-11 17:04:41 +0000

    added setup file and refactored tests (#209)

--- main/src/__tests__/components/footer.test.js
+++ main/src/__tests__/components/footer.test.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'enzyme';
 
 import Footer from '../../components/footer';
-
-import { createRender } from '@material-ui/core/test-utils';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -16,7 +11,6 @@ it('renders without crashing', () => {
 });
 
 it('snapshot of initial component', () => {
-  let render = createRender();
   const component = render(<Footer />);
   expect(component).toMatchSnapshot();
 });
--- main/src/__tests__/components/header.test.js
+++ main/src/__tests__/components/header.test.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'enzyme';
 
 import Header from '../../components/header';
-
-import { createRender } from '@material-ui/core/test-utils';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -16,7 +11,6 @@ it('renders without crashing', () => {
 });
 
 it('snapshot of initial component', () => {
-  let render = createRender();
   const component = render(<Header />);
   expect(component).toMatchSnapshot();
 });
