5258a581be7a4c6d1c14c5939403de6daaad22b6
Shawn <sppang1@yahoo.com>
2018-12-10 19:15:14 -0500

    added test for rendering divs

--- main/src/__tests__/App.test.js
+++ main/src/__tests__/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from '../App';
 
 import { createRender } from '@material-ui/core/test-utils';
-import { configure } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
@@ -19,4 +18,10 @@ it('snapshot changes when input change', () => {
   let render = createRender();
   const component = render(<App />);
   expect(component).toMatchSnapshot();
+});
+
+it('always renders a div', () => {
+  const wrapper = mount(<App />);
+  const divs = wrapper.find('div');
+  expect(divs.length).toBeGreaterThan(0);
 });
