98f357268cb1cedd73b4fb9879794085e6db6be5
Luke Taylor <lmcjt@outlook.com>
2018-12-12 16:20:48 +0000

    updated tests for header and footer, wip on app.test.js

--- main/src/__tests__/App.test.js
+++ main/src/__tests__/App.test.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { render, mount } from 'enzyme';
 
 import App from '../App';
+import Header from '../components/header.js';
+import Footer from '../components/footer.js';
+import FilterBar from '../components/filterBar.js';
+import Card from '../components/card.js';
+import MultiCard from '../components/multiCard';
+import ErrorIcon from '@material-ui/icons/Error';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('render all initial child components', () => {
+  const wrapper = mount(<App />);
+
+  expect(wrapper.find(Header)).toHaveLength(1);
+  expect(wrapper.find(FilterBar)).toHaveLength(1);
+  expect(wrapper.find('main')).toHaveLength(1);
+  expect(wrapper.find(Footer)).toHaveLength(1);
+  expect(wrapper.find(Card).length).toBeGreaterThan(0);
+  expect(wrapper.find(MultiCard).length).toBeGreaterThan(0);
+  expect(wrapper.find(ErrorIcon)).toHaveLength(0);
+});
+
+xit('renders error component for no search results', () => {
+  const wrapper = mount(<App />);
+
+  wrapper.setState({
+    search: true,
+    content: []
+  });
+
+  expect(wrapper.find(ErrorIcon)).toHaveLength(1);
 });
 
 it('snapshot changes when input change', () => {
   const component = render(<App />);
   expect(component).toMatchSnapshot();
-});
-
-it('always renders a div', () => {
-  const wrapper = mount(<App />);
-  const divs = wrapper.find('div');
-  expect(divs.length).toBeGreaterThan(0);
 });
--- main/src/__tests__/components/header.test.js
+++ main/src/__tests__/components/header.test.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 
 import Header from '../../components/header';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import SearchIcon from '@material-ui/icons/Search';
+import TuneIcon from '@material-ui/icons/Tune';
+import CloseIcon from '@material-ui/icons/HighlightOff';
+import Autocomplete from '../../components/autocomplete';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Header />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('render all initial child components', () => {
+  const wrapper = mount(<Header autoComplete={[]} />);
+
+  expect(wrapper.find(AppBar)).toHaveLength(1);
+  expect(wrapper.find(Toolbar)).toHaveLength(1);
+  expect(wrapper.find(Typography)).toHaveLength(1);
+  expect(wrapper.find(SearchIcon)).toHaveLength(1);
+  expect(wrapper.find(Autocomplete)).toHaveLength(1);
+  expect(wrapper.find(IconButton)).toHaveLength(1);
+  expect(wrapper.find(TuneIcon)).toHaveLength(1);
+});
+
+it('changes icon when showFilters props changes', () => {
+  const wrapper = mount(<Header autoComplete={[]} />);
+
+  expect(wrapper.find(IconButton)).toHaveLength(1);
+  expect(wrapper.find(TuneIcon)).toHaveLength(1);
+
+  wrapper.setProps({ showFilters: true });
+
+  expect(wrapper.find(TuneIcon)).toHaveLength(0);
+  expect(wrapper.find(CloseIcon)).toHaveLength(1);
 });
 
 it('snapshot of initial component', () => {
-  const component = render(<Header />);
+  const component = render(<Header autoComplete={[]} />);
   expect(component).toMatchSnapshot();
 });
